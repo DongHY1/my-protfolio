d10d05d7cd3e3f8cbbe629295b37d32ede41967f
DongHY1 <915110101@qq.com>
2022-03-28 17:49:27 +0800

    增加dat.gui

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import "./style.css";
-
+import * as dat from "dat.gui";
 import {
   Scene,
   PerspectiveCamera,
@@ -9,15 +9,49 @@ import {
   Mesh,
   DoubleSide,
   DirectionalLight,
-  FlatShading
+  FlatShading,
 } from "three";
+// 创建GUI
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments:10,
+    heightSegments:10
+  },
+};
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "widthSegments", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "heightSegments", 1, 20).onChange(generatePlane);
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+  const { array } = planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+    array[i + 2] = z + Math.random();
+  }
+}
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new WebGLRenderer();
-const planeGeometry = new PlaneGeometry(5, 5, 10, 10);
+const planeGeometry = new PlaneGeometry(5, 10, 10, 10);
 // 双层平面
-const planeMaterial = new MeshPhongMaterial({ color: 0xff0000,side:DoubleSide,flatShading:FlatShading});
+const planeMaterial = new MeshPhongMaterial({
+  color: 0xff0000,
+  side: DoubleSide,
+  flatShading: FlatShading,
+});
 const planeMesh = new Mesh(planeGeometry, planeMaterial);
 renderer.setSize(innerWidth, innerHeight);
 // 减少锯齿
@@ -25,15 +59,15 @@ renderer.setPixelRatio(devicePixelRatio);
 scene.add(planeMesh);
 camera.position.z = 5;
 
-const light = new DirectionalLight(0xffffff,1)
-light.position.set(0,-1,1)
-scene.add(light)
-const {array} = planeMesh.geometry.attributes.position
-for(let i=0;i<array.length;i+=3){
-   const x = array[i]
-   const y = array[i+1]
-   const z = array[i+2]
-   array[i+2] = z+Math.random()
+const light = new DirectionalLight(0xffffff, 1);
+light.position.set(0, -1, 1);
+scene.add(light);
+const { array } = planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+  array[i + 2] = z + Math.random();
 }
 
 function animate() {
