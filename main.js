ef8b54dd2b8a455b5231d360bee7a0fe45c6b0ff
DongHY1 <915110101@qq.com>
2022-03-28 17:14:19 +0800

    创建平面

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ import {
   PerspectiveCamera,
   WebGLRenderer,
   PlaneGeometry,
-  BoxGeometry,
-  MeshBasicMaterial,
+  MeshPhongMaterial,
   Mesh,
   DoubleSide,
+  DirectionalLight,
+  FlatShading
 } from "three";
 
 const scene = new Scene();
@@ -16,17 +17,29 @@ const camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new WebGLRenderer();
 const planeGeometry = new PlaneGeometry(5, 5, 10, 10);
 // 双层平面
-const planeMaterial = new MeshBasicMaterial({ color: 0xff0000,side:DoubleSide });
+const planeMaterial = new MeshPhongMaterial({ color: 0xff0000,side:DoubleSide,flatShading:FlatShading});
 const planeMesh = new Mesh(planeGeometry, planeMaterial);
 renderer.setSize(innerWidth, innerHeight);
 // 减少锯齿
 renderer.setPixelRatio(devicePixelRatio);
 scene.add(planeMesh);
 camera.position.z = 5;
+
+const light = new DirectionalLight(0xffffff,1)
+light.position.set(0,-1,1)
+scene.add(light)
+const {array} = planeMesh.geometry.attributes.position
+for(let i=0;i<array.length;i+=3){
+   const x = array[i]
+   const y = array[i+1]
+   const z = array[i+2]
+   array[i+2] = z+Math.random()
+}
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  planeMesh.rotation.x += 0.01;
+  // planeMesh.rotation.x += 0.01;
 }
 animate();
 
